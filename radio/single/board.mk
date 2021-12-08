002ecb564ecc20b2085ac4af330791d7fdd5a81c
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-12-07 20:39:59 -0500

    Revert "msm8916-common: Moving TARGET_LD_SHIM_LIBS to fragment & update libsec-ril.*.so location"
    This reverts commit adc93b0a8eb3637b39e81df9ca579404504655fc.

--- radio/single/board.mk
+++ radio/single/board.mk
@@ -3,7 +3,3 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/radio/single/system.prop
 
 # Radio/RIL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/radio/single/manifest.xml
-
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib/libsec-ril.so|libshim_secril.so
