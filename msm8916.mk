e8922673cc9e312a436610b44d8956a18cf1fcb5
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-04-20 11:35:15 -0600

    fixup! msm8916-common: decommonize a2dp_in_audio_policy_configuration.xml
    Need a blank line in between the \ and next line otherwise compiler thinks the
    entire section below is commented out and thus volume rocker doesn't work
    and sound is at maximum.
    
    The SM-T35x variants need a specific a2dp_in_audio_policy_configuration.xml
    due to it's proprietary NXP speaker amp implementation.
    
    So we need to make this a2dp_in_audio_policy_configuration.xml device
    specific.

--- msm8916.mk
+++ msm8916.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration file
 # decommonize     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
