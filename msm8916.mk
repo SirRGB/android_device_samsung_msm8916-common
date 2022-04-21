397f598549fa392910b0aad5ecc6391636a45488
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-04-21 12:40:32 -0600

    msm8916-common: decommonize vendor.lineage.livedisplay
    Decommonize livedisplay as it doesn't work for SM-T560NU in 18.1 and higher.
    This needs to be in device specific tree otherwise SM-T560NU will not work
    after OS is booted and adb logs show same for IDisplayModes.
    
    hwservicemanager Since vendor.lineage.livedisplay@2.0::IReadingEnhancement/default is not registered, trying to start it as a lazy HAL.
    HidlServiceManagement getService: Trying again for vendor.lineage.livedisplay@2.0::IReadingEnhancement/default...
    libc     Unable to set property "ctl.interface_start" to "vendor.lineage.livedisplay@2.0::IReadingEnhancement/default": error code: 0x20
    hwservicemanager Tried to start vendor.lineage.livedisplay@2.0::IReadingEnhancement/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    init: Control message: Could not find 'vendor.lineage.livedisplay@2.0::IReadingEnhancement/default' for ctl.interface_start from pid: 224 (/system/bin/hwservicemanager)
    HidlServiceManagement Waited one second for vendor.lineage.livedisplay@2.0::IReadingEnhancement/default

--- msm8916.mk
+++ msm8916.mk
@@ -112,8 +112,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8916 \
     libtinyxml \
     libtinyxml2 \
-    memtrack.msm8916 \
-    vendor.lineage.livedisplay@2.0-service.samsung-qcom
+    memtrack.msm8916
 
 # DRM
 PRODUCT_PACKAGES += \
