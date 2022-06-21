9e7ed64ce1c86d5c5db691a9f92d19ec6b0c8e3f
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-06-20 19:17:50 -0600

    msm8916-common: decommonize android.hardware.audio.service
    We need to decommonize android.hardware.audio.service because after the June 5th, 2022
    security patches, this service crashes on SM-T35x devices that use audio hal 2.0.  However,
    it's needed by the other msm8916 devices, so this entry needs to be in the specific
    device tree.
    
    DEBUG   : *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    DEBUG   : Build fingerprint: 'samsung/gt58wifixx/gt58wifi:7.1.1/NMF26X/T350XXU1CQJ5:user/release-keys'
    DEBUG   : Revision: '4'
    DEBUG   : ABI: 'arm'
    DEBUG   : Timestamp: 1974-12-18 15:13:53.736955798-0700
    DEBUG   : Process uptime: 0s
    DEBUG   : Cmdline: /vendor/bin/hw/android.hardware.audio.service
    DEBUG   : pid: 357, tid: 357, name: android.hardwar  >>> /vendor/bin/hw/android.hardware.audio.service <<<
    DEBUG   : uid: 1041
    DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    DEBUG   : Abort message: 'Could not register Audio Core API'
    DEBUG   :     r0  00000000  r1  00000165  r2  00000006  r3  bec01e50
    DEBUG   :     r4  bec01e64  r5  bec01e48  r6  00000165  r7  0000016b
    DEBUG   :     r8  bec01e50  r9  bec01e60  r10 bec01e80  r11 bec01e70
    DEBUG   :     ip  00000165  sp  bec01e20  lr  ab139029  pc  ab13903c
    DEBUG   : backtrace:
    DEBUG   :       #00 pc 0006103c  /apex/com.android.runtime/lib/bionic/libc.so (abort+172) (BuildId: d3fb94403256fae9e9e9a2dd426e0b9f)
    DEBUG   :       #01 pc 0000485f  /system/lib/liblog.so (__android_log_default_aborter+6) (BuildId: a68126762dcfe71950c517681f7188bd)
    DEBUG   :       #02 pc 00004f33  /system/lib/liblog.so (__android_log_assert+158) (BuildId: a68126762dcfe71950c517681f7188bd)
    DEBUG   :       #03 pc 00002607  /system/vendor/bin/hw/android.hardware.audio.service (main+2374) (BuildId: fa2bf9c3c664c1482845161c1b47d442)
    DEBUG   :       #04 pc 0005a72b  /apex/com.android.runtime/lib/bionic/libc.so (__libc_init+54) (BuildId: d3fb94403256fae9e9e9a2dd426e0b9f)
    
    Luckily, SM-T35x variants which require audio 2.0 hal still work without this service.

--- msm8916.mk
+++ msm8916.mk
@@ -35,7 +35,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8916 \
