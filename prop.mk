ab31d92af6d00a4044a19b82f72a25f41afb22d9
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-07-29 21:23:55 -0600

    msm8916-common: remove ro.adb.secure prop because it's not allowed in userdebug build.
    Compiler complains if we build an userdebug build with
    
    error: found duplicate sysprop assignments:
    ro.adb.secure=1
    ro.adb.secure=0
    
    However, having this prop left in works for eng builds.

--- prop.mk
+++ prop.mk
@@ -1,6 +1,5 @@
 # 19.0 bringup - turn on all adbd right away
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0 \
     ro.debuggable=1 \
     persist.service.adb.enable=1 \
     persist.sys.usb.config=adb
